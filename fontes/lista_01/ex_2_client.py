0e082e81268b58c12f6ff069c667382f522d7eef
JonesQuito <jonesdhy@hotmail.com>
2019-09-20 02:51:42 -0300

    Alterações no cliente e no serve do exercicio 2

--- fontes/lista_01/ex_2_client.py
+++ fontes/lista_01/ex_2_client.py
@@ -1,18 +1,22 @@
 import socket
 import pickle
 
-from models.pessoa import Pessoa
-from utils.connectSockt import ClienteSocket
+from models import Pessoa
+from utils import ClienteSocket
 
-pessoa = Pessoa('Leandra', 25, 'f')
-pessoa = pickle.dumps(pessoa)
 
+# Prepara os dados para envio
+pessoa = Pessoa('Jones', 17, 'm')
+dados = {'op': 'maioridade', 'dados':pessoa}
+dados = pickle.dumps(dados)
+
+# Instancia um cliente socket e envia os dados
 cliente = ClienteSocket('localhost', 3000)
-cliente.enviar(pessoa)
+cliente.enviar(dados)
 
-print('antes de conectar!')
+
 resposta = pickle.loads(cliente.recebe())
 if resposta:
-    print(pickle.loads(pessoa).nome, 'é maior de idade')
+    print('É maior de idade')
 else:
-    print(pickle.loads(pessoa).nome, 'é menor de idade')
+    print('É menor de idade')
